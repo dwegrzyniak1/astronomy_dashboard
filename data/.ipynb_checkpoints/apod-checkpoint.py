de12855fe589b1989fd6f17ace37206f47914825
dwegrzyniak1 <dominikawegrzyniak111@gmail.com>
2025-08-06 09:10:00 +0200

    APOD function done

--- data/.ipynb_checkpoints/apod-checkpoint.py
+++ data/.ipynb_checkpoints/apod-checkpoint.py
@@ -2,9 +2,21 @@ import requests
 
 #function to retrieve the nasa photo of the dat
 def get_apod(api_key):
+#connect to nasa api
     response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}")
-    print(response.status_code)
-    print(response.json())
 
+    #check if connection is succesfull
+    if response.status_code != 200:
+        raise RuntimeError(f"Error {response.status_code}: {response.text}")
+
+    #parse json
+    data = response.json()
+
+    image_url = data['hdurl'] or data["url"]
     
-    return 3;
+    return {
+        "date" : data["date"]
+        ,"title" : data["title"]
+        ,"explanation" : data["explanation"]
+        ,"img_url" : image_url
+    }
